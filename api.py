631ee7005fd5e0760de2b605c91af47d6e32eb63
Johnny19Yuan <johnny19yuan@gmail.com>
2025-12-23 09:11:48 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,1 +1,9 @@
-requests.get(@67)
+import requests
+
+def bus(data):
+    response = requests.get(f"https://bustime.mta.info/api/2/siri/stop-monitoring.json?key=b37b2029-5bb8-43dc-bb85-4bc15a1de888&MTA&MonitoringRef=200700&LineRef=MTA%20NYCT_S53&MaximumStopVisits=10")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    
+bus(1)
