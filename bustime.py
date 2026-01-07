5f2bee417ce5c296d190e6045d8636ecd016db94
Johnny19Yuan <johnny19yuan@gmail.com>
2026-01-06 22:54:13 -0500

    bus

--- bustime.py
+++ bustime.py
@@ -1,11 +1,11 @@
 import requests
 
-def bus():
-    response = requests.get(f"https://bustime.mta.info/api/2/siri/stop-monitoring.json?key=b37b2029-5bb8-43dc-bb85-4bc15a1de888&MTA&MonitoringRef={201020}&MaximumStopVisits=100")
+def bus(stop):
+    response = requests.get(f"https://bustime.mta.info/api/2/siri/stop-monitoring.json?key=b37b2029-5bb8-43dc-bb85-4bc15a1de888&MTA&MonitoringRef={stop}&MaximumStopVisits=100")
     data = response.json()
     print({
         "route": data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"][0]["MonitoredVehicleJourney"]["PublishedLineName"],
-    
+        "dest" : data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"][0]["MonitoredVehicleJourney"]["DestinationName"]
     })
     
-bus()
+bus(201020)
