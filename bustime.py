a067410f3580f0f57028409f0241623875fc1a84
Johnny19Yuan <johnny19yuan@gmail.com>
2026-01-07 09:04:03 -0500

    Update bustime.py

--- bustime.py
+++ bustime.py
@@ -3,9 +3,14 @@ import requests
 def bus(stop):
     response = requests.get(f"https://bustime.mta.info/api/2/siri/stop-monitoring.json?key=b37b2029-5bb8-43dc-bb85-4bc15a1de888&MTA&MonitoringRef={stop}&MaximumStopVisits=100")
     data = response.json()
-    print({
+    return {
         "route": data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"][0]["MonitoredVehicleJourney"]["PublishedLineName"],
-        "dest" : data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"][0]["MonitoredVehicleJourney"]["DestinationName"]
-    })
-    
-bus(201020)
+        "dest" : data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"][0]["MonitoredVehicleJourney"]["DestinationName"],
+        "exarrtime": data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"][0]["MonitoredVehicleJourney"]["MonitoredCall"]["ExpectedArrivalTime"],
+        "currtime": data["Siri"]["ServiceDelivery"]["ResponseTimestamp"],
+        "distance": data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"][0]["MonitoredVehicleJourney"]["MonitoredCall"]["Extensions"]["Distances"]["PresentableDistance"],
+        "passcap": data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"][0]["MonitoredVehicleJourney"]["MonitoredCall"]["Extensions"]["Capacities"]["EstimatedPassengerCount"],
+    }
+  
+bustime = bus(201020)
+print(bustime)
