48a740097afd9f3160d66f45f185ea3cebf0b8b5
Johnny19Yuan <johnny19yuan@gmail.com>
2026-01-09 09:26:11 -0500

    tk

--- bustime copy.py
+++ bustime copy.py
@@ -2,6 +2,26 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title("Bustime")
-window.geometry("1920x1080")
+window.geometry("1024x728")
+btn = tk.Button(
+    window,
+    text="see more",
+    width=120,
+    height=2,
+    font=("Impact", 32),
+    bg="black",
+    fg="white",
+    activebackground="gray",
+    padx=10,
+    pady=5
+)
+btn.pack(pady=20)
+label = tk.Label(
+    window,
+    text="Welcome to mta bus time enhanced™ super editon®",
+    font=("Comic Sans MS", 14),
+    fg="blue"
+)
+label.pack(pady=10)
 
 window.mainloop()
--- bustime.py
+++ bustime.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import requests
 
 def bus(stop):
-    response = requests.get(f"https://bustime.mta.info/api/2/siri/stop-monitoring.json?key=3f6c967c-b46d-4e88-b0b7-4d35e00b8c77&MTA&MonitoringRef={stop}&MaximumStopVisits=100")
+    response = requests.get(f"https://bustime.mta.info/api/2/siri/stop-monitoring.json?key=fad509a0-390b-44d5-a448-7d9bd3ca1bdb&MTA&MonitoringRef={stop}&MaximumStopVisits=100")
     data = response.json()
     return {
         "route": data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"][0]["MonitoredVehicleJourney"]["PublishedLineName"],
