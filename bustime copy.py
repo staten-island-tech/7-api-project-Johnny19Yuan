b09ef0daf986f7c5cdb795207183e65f66c332e4
Johnny19Yuan <johnny19yuan@gmail.com>
2026-01-14 09:25:28 -0500

    w

--- bustime copy.py
+++ bustime copy.py
@@ -2,26 +2,39 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title("Bustime")
-window.geometry("1168x728")
+window.geometry("1166x768")
 btn = tk.Button(
     window,
-    text="SEE MORE",
-    width=120,
+    text="Enter",
+    width=10,
     height=2,
-    font=("Impact", 32),
+    font=("Helvetica", 32, "bold"),
     bg="black",
     fg="white",
     activebackground="gray",
     padx=10,
     pady=5
-)
-btn.pack(pady=20)
+) 
+btn.grid(row=10, column=0, padx=10, pady=20)
 label = tk.Label(
     window,
     text="Welcome to mta bus time enhanced™ super editon®",
     font=("Comic Sans MS", 14),
-    fg="blue"
+    fg="red"
 )
-label.pack(pady=10)
+label.grid(row=1, column=0, pady=10)
 
+def search():
+    print(entry.get)
+
+
+entry = tk.Entry(window, width=30)
+button = tk.Button(window, text="Search", command=search)
+
+entry.grid(row=0, column=0, padx=10, pady=20)
+button.grid(row=0, column=1, padx=2, pady=20)
+
+
+label.grid(pady=10)
+print(entry.get)
 window.mainloop()
--- e.py
+++ e.py
@@ -1,4 +1,3 @@
-import tkinter as tk
 import requests
 def bus(stop,x):
     response = requests.get(f"https://bustime.mta.info/api/2/siri/stop-monitoring.json?key=fad509a0-390b-44d5-a448-7d9bd3ca1bdb&MTA&MonitoringRef={stop}&MaximumStopVisits=100")
@@ -11,46 +10,5 @@ def bus(stop,x):
         "distance": data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"][x]["MonitoredVehicleJourney"]["MonitoredCall"]["Extensions"]["Distances"]["PresentableDistance"],
         
     }
-window = tk.Tk()
-window.title("Bustime")
-window.geometry("1166x768")
-btn = tk.Button(
-    window,
-    text="Enter",
-    width=10,
-    height=2,
-    font=("Helvetica", 32, "bold"),
-    bg="black",
-    fg="white",
-    activebackground="gray",
-    padx=10,
-    pady=5
-) 
-btn.grid(row=10, column=0, padx=10, pady=20)
-label = tk.Label(
-    window,
-    text="Welcome to mta bus time enhanced™ super editon®",
-    font=("Comic Sans MS", 14),
-    fg="red"
-)
-label.grid(row=1, column=0, pady=10)
 
-def search():
-    bus(entry.get,0)
-
-
-entry = tk.Entry(window, width=30)
-button = tk.Button(window, text="Search", command=search)
-
-entry.grid(row=0, column=0, padx=10, pady=20)
-button.grid(row=0, column=1, padx=2, pady=20)
-
-
-label.grid(pady=10)
-print(entry.get)
-window.mainloop()
-
-
-
-#201020 
-
+print(bus(200700,0))
